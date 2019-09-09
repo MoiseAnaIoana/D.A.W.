32be537eb3f5efd8cddc5442bd620af0a2d229d7
MoiseAnaIoana <44284743+MoiseAnaIoana@users.noreply.github.com>
2019-09-10 00:32:57 +0300

    Finished MVC (layout+model Binding+Validation)

--- DutchTreat/DutchTreat/Controllers/AppController.cs
+++ DutchTreat/DutchTreat/Controllers/AppController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DutchTreat.Services;
+using DutchTreat.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,47 @@ using System.Threading.Tasks;
 
 namespace DutchTreat.Controllers
 {
-    public class AppController: Controller
+    public class AppController : Controller
     {
-        public IActionResult Index()
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
         {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                _mailService.SendMessage("moiseana100@yahoo.com", model.Subject, $"Form:{model.Email}, Message: ");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+
             return View();
         }
     }
 }
+
